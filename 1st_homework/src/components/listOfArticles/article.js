8bd7131a33b4353c8c35ab2a7a3aeceac55b5d37
Nikita-Kukenko <kukenkonik@gmail.com>
2020-10-30 16:56:20 +0200

    refactoring code

--- 1st_homework/src/components/listOfArticles/article.js
+++ 1st_homework/src/components/listOfArticles/article.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Article(props) {
   return (
     <div style={props.style}>
-      <h1>{props.content.title}</h1>
+      <h2>{props.content.title}</h2>
+      <hr />
       <p>{props.content.text}</p>
     </div>
   );
--- 1st_homework/src/components/listOfArticles/index.js
+++ 1st_homework/src/components/listOfArticles/index.js
@@ -3,7 +3,7 @@ import Article from './article.js'
 
 let defaultArticle = {
   title: 'Default Title',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.',
 }
 
 let errorArticle = {
@@ -13,45 +13,45 @@ let errorArticle = {
 
 let successArticle = {
   title: 'Success Title',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quos praesentium placeat natus illo facere et ex repellendus porro consectetur necessitatibus quis aspernatur ea, vel assumenda vero rerum ipsum doloribus.',
 }
 
 let errorArticleStyle = {
   color: 'red',
-  width: '33%',
+  width: '50%',
   height: 'auto',
   backgroundColor: 'transparent',
   border: '3px solid red',
-  margin: '20px 20px 0 0',
+  margin: '20px auto',
   borderRadius: '5px',
-  padding: '10px'
+  padding: '10px',
 }
 
 let defaultArticleStyle = {
   color: 'blue',
-  width: '33%',
+  width: '50%',
   height: 'auto',
   backgroundColor: 'transparent',
   border: '3px solid blue',
-  margin: '20px 20px 0 0',
+  margin: '20px auto',
   borderRadius: '5px',
-  padding: '10px'
+  padding: '10px',
 }
 
 let successArticleStyle = {
   color: 'green',
-  width: '33%',
+  width: '50%',
   height: 'auto',
   backgroundColor: 'transparent',
   border: '3px solid green',
   borderRadius: '5px',
-  margin: '20px 0 0 0',
-  padding: '10px'
+  margin: '20px auto',
+  padding: '10px',
 }
 
 function ListOfArticles() {
   return (
-    <div style={{display: 'flex'}}>
+    <div>
       <Article content={defaultArticle} style={defaultArticleStyle} />
       <Article content={errorArticle} style={errorArticleStyle} />
       <Article content={successArticle} style={successArticleStyle} />
